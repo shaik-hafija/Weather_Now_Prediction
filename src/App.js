940be20a52ee685d09cc681e7e61200349bd75e9
shaik-hafija <hafijashaik7876@gmail.com>
2024-11-25 15:27:50 +0530

    Weather_Now

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Dashboard from './components/dashboard/dashboard';
+// import Home from './components/home/home';
+function App() {
+  return (
+    <>
+      <Dashboard />
+      {/* <Home /> */}
+    </>
+  );
+}
+
+export default App;
